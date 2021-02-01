d32c670c94fde0a1434a9d072e5a2c91e3c5eec1
Sho Tsuboya <sho_825.sa216@docomo.ne.jp>
2021-02-01 18:28:31 +0900

    just finished first view

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,6 @@
 class Tweet < ApplicationRecord
 end
+
+# createメソッドがTweetモデルに対して使えるのは
+# 恐らくApplicationRecordクラスが所有しているクラスメソッドのcreateメソッドにDBをいじるロジックが組まれている。
+# Tweetモデルはそのクラスを継承している。
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tweets, only: :index
 end
+
+# 解説 
+# tweetsというパスにアクセスすると、tweetsコントローラーのindexアクションを実行。
